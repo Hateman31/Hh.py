e9fe50a60bca7b484469b40fa1568733b9845bf0
Hateman31 <metilogrunge31@gmail.com>
2015-04-07 11:40:42 +0300

    Update api.py

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ class API:
 		r = urllib.request.urlopen(base+query)
 		return json.loads(r.read().decode('utf8'))
 	def vacancies(params):
+		''' Функция поиска по вакансиям''
 		return self.query('vacancies/'+params)
 		#написать функцию перевода словаря params в get-запрос
 		#{ key1:value1,key2:value2 }  ==> <uri>?key1=value1&key2=value2
